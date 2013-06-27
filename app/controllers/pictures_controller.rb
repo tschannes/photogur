33dd6053ce7d47a0425503510c52b45eadb0500b
tschannes <mail@tscherrig.net>
2013-06-27 12:01:24 -0400

    routes fixed

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,6 +1,48 @@
 class PicturesController < ApplicationController
 
 	def index
-		
+	    @pictures = Picture.all
 	end
+	  
+	def new
+	  @picture = Picture.new
+	end
+
+	def create
+	  # Make the variable @picture a new picture with the given params
+	  params.permit!
+	  @picture = Picture.new(params[:picture])
+	  if @picture.save
+	    # if the save for the picture was successful, go to index.html.erb
+	    redirect_to pictures_url
+	  else
+	    # otherwise render the view associated with the action :new (i.e. new.html.erb)
+	    render :new
+	  end
+	end
+
+	def show
+	    @picture = Picture.find(params[:id])
+	end
+
+	def edit
+	  @picture = Picture.find(params[:id])
+	end
+
+	def update
+	  @picture = Picture.find(params[:id])
+	  params.permit!
+	  if @picture.update_attributes(params[:picture])
+	    redirect_to "/pictures/#{@picture.id}"
+	  else
+	    render :edit
+	  end
+	end
+
+	def destroy
+	  @picture = Picture.find(params[:id])
+	  @picture.destroy
+	  redirect_to pictures_url
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,56 +1,83 @@
-Photogur::Application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+# Photogur::Application.routes.draw do
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+#   root :to => "pictures#index"
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+#   get 'pictures', to: 'pictures#index'
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+#   post 'pictures', to: "pictures#create"
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+#   get 'pictures/:id', to: 'pictures#show', as: "picture"
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
+#   get 'pictures/:id/edit', to: "pictures#edit", as: "edit_picture"
   
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+#   get 'pictures/new', to: 'pictures#new'
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+#   # get 'pictures/:id' => 'pictures#show', as: "picture"
+#   # The priority is based upon order of creation: first created -> highest priority.
+#   # See how all your routes lay out with "rake routes".
+
+#   # You can have the root of your site routed with "root"
+#   # root 'welcome#index'
+
+#   # Example of regular route:
+#   #   get 'products/:id' => 'catalog#view'
+
+#   # Example of named route that can be invoked with purchase_url(id: product.id)
+#   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+
+#   # Example resource route (maps HTTP verbs to controller actions automatically):
+#   #   resources :products
+
+#   # Example resource route with options:
+#   #   resources :products do
+#   #     member do
+#   #       get 'short'
+#   #       post 'toggle'
+#   #     end
+#   #
+#   #     collection do
+#   #       get 'sold'
+#   #     end
+#   #   end
+
+#   # Example resource route with sub-resources:
+#   #   resources :products do
+#   #     resources :comments, :sales
+#   #     resource :seller
+#   #   end
+
+#   # Example resource route with more complex sub-resources:
+#   #   resources :products do
+#   #     resources :comments
+#   #     resources :sales do
+#   #       get 'recent', on: :collection
+#   #     end
+#   #   end
+  
+#   # Example resource route with concerns:
+#   #   concern :toggleable do
+#   #     post 'toggle'
+#   #   end
+#   #   resources :posts, concerns: :toggleable
+#   #   resources :photos, concerns: :toggleable
+
+#   # Example resource route within a namespace:
+#   #   namespace :admin do
+#   #     # Directs /admin/products/* to Admin::ProductsController
+#   #     # (app/controllers/admin/products_controller.rb)
+#   #     resources :products
+#   #   end
+# end
+
+Photogur::Application.routes.draw do
+  # post 'pictures' => "pictures#create"
+
+  # get 'pictures' => 'pictures#index'
+  # get 'pictures/new' => 'pictures#new'
+  # # patch 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+  # get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+  # delete 'pictures/:id/destroy' => "pictures#destroy", as: "destroy_picture"
+  # get 'pictures/:id' => 'pictures#show', as: "picture"
+  resources :pictures
+  root :to => "pictures#index"
 end
