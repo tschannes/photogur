fbf8e7eb4bbefd8a36475da614decb03a5865fac
tschannes <mail@tscherrig.net>
2013-06-28 11:02:34 -0400

    empty form submission fixed

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -13,7 +13,9 @@ class PicturesController < ApplicationController
 	  params.permit!
 	  @picture = Picture.new(params[:picture])
 	 # && if params[:picture].empty? == false
-	  if @picture.save  
+	  if @picture[:url].blank? || @picture[:artist].blank? || @picture[:title].blank?
+	  	render :new
+	  elsif @picture.save  
 	    # if the save for the picture was successful, go to index.html.erb
 	    redirect_to pictures_url
 	  else
