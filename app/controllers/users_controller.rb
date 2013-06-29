a9c7ee82893f226bc8c6bb5491681135fd20cd4e
tschannes <mail@tscherrig.net>
2013-06-29 00:17:49 -0400

    login added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,20 @@ class UsersController < ApplicationController
 		@users = User.all.sort_by_email
 	end
 
-	# def new
-	# 	@user = User.new
-	# end
+	def new
+		@user = User.new
+	end
+
+	def create
+		params.permit!
+		@user = User.new(params[:user])
+		if @user.save
+			flash[:success] = "Welcome to the Sample App!"
+			redirect_to @user
+		else
+			render 'new'
+		end
+	end
 
 	def show
 		@user = User.find(params[:id])
