31bdf55106f029b3ec5dffec0995e08cd1a12ddb
tschannes <mail@tscherrig.net>
2013-06-30 12:55:11 -0400

    sessions helper added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,7 @@ class SessionsController < ApplicationController
 	def create 
 		user = User.find_by_email(params[:session][:email])
 		if user && user.authenticate(params[:session][:password])
-			sign_in user
-			redirect_to user
+			sign_in
 		else
 			flash.now[:error] = 'Invalid email/password combination'
 			render 'new' 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130628220429) do
+ActiveRecord::Schema.define(version: 20130630162940) do
 
   create_table "pictures", force: true do |t|
     t.string   "artist"
